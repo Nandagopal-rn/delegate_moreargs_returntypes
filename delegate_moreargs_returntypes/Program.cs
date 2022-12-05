ba400dd69a062c96dbb9891001afe2a9491c40ee
nandagopal.rn <nandagopal.rn@PIA3278>
2022-12-05 14:09:58 +0530

    Add project files.

---
+++ delegate_moreargs_returntypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace delegate_moreargs_returntypes
+{
+
+    public delegate int DelMoreArgAndReturn(int firstnumber, int secondnumber,int thirdnumber);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var MoreArgsandReturn = new DelMoreArgAndReturn(MoreArgAndReturnType);
+            Console.WriteLine(MoreArgsandReturn.Invoke(5,5,5));
+        }
+
+        public static int MoreArgAndReturnType(int firstnumber, int secondnumber, int thirdnumber)
+        {
+            return (firstnumber + secondnumber + thirdnumber);
+        }
+    }
+}
