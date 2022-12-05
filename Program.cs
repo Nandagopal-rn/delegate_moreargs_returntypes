06dc96b0d82f52d0ecdbaa0e44390e0ef4279e09
Nandagopal-rn <119663637+Nandagopal-rn@users.noreply.github.com>
2022-12-05 14:12:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace delegate_more_argument
+{
+
+    public delegate void DelWithMoreArg(string firstname, string lastname);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var moreArgments = new DelWithMoreArg(MoreArgs);
+            moreArgments.Invoke("Marthanda","varma");
+        }
+
+        public static void MoreArgs(string firstname, string lastname)
+        {
+            Console.WriteLine($"Welcome {firstname} {lastname}");
+        }
+    }
+}
